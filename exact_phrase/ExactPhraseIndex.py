018e577d455b8ab39d2cccb26bffb878feec30f9
Alessandro Marin <alessandro@studocu.com>
2017-01-27 15:59:21 +0100

    makes search possible

--- exact_phrase/ExactPhraseIndex.py
+++ exact_phrase/ExactPhraseIndex.py
@@ -1,11 +1,37 @@
+from PhraseTree import *
+
 class ExactPhraseIndex:
     def __init__(self, phrases):
         self.phrase_tree = PhraseTree(phrases)
 
-    def _match(self, tokens, phrase, result = []):
-        if not tokens:
+    def _find_matches(self, tokens, phrase = [], result = []):
+        if not tokens and not phrase:
             return result
-        elif 
+        elif phrase:
+            has_phrase = self.phrase_tree.has(phrase)
 
-    def match(self, text):
-        tokens = text.split(' ')
+            if has_phrase == 1:
+                result.append(' '.join(phrase))
+                phrase.pop(0)
+            elif has_phrase == -1:
+                phrase.pop(0)
+            elif has_phrase == 0 and tokens:
+                phrase.append(tokens.pop(0))
+        else:
+            phrase.append(tokens.pop(0))
+
+        return self._find_matches(tokens, phrase, result)
+
+    def matches(self, text):
+        tokens = text.lower().split(' ')
+        return self._find_matches(tokens)
+
+idx = ExactPhraseIndex([
+        "I am a pony",
+        "I am a human child",
+        "I am now a baby",
+        "I am not your type",
+        "a baby boy"
+    ])
+
+print idx.matches("Hi my name is george I am not a pony but I am a human I am now a baby and a baby boy")
--- exact_phrase/PhraseTree.py
+++ exact_phrase/PhraseTree.py
@@ -5,7 +5,7 @@ class PhraseTree:
 
     def construct(self, phrases):
         for phrase in phrases:
-            self.add_phrase(self.tree, phrase.split(' '))
+            self.add_phrase(self.tree, phrase.lower().split(' '))
 
     def add_phrase(self, tree, phrase):
         """
@@ -23,13 +23,22 @@ class PhraseTree:
             return self.add_phrase(tree[word], phrase)
 
     def _has(self, tree, phrase):
+        """
+            Returns:
+                a) 1 if the tree matches a phrase
+                b) 0 if the tree has the phrase so far but it's not complete
+                c) -1 if the tree does not have the phrase 
+        """
         if not phrase:
-            return '_M' in tree and tree['_M']
+            if '_M' in tree and tree['_M']:
+                return 1
+            else:
+                return 0
         else:
             word = phrase.pop(0)
             if word in tree:
                 return self._has(tree[word], phrase)
-            else: return False
+            else: return -1
 
     def has(self, phrase):
-        return self._has(self.tree, phrase.split(' '))
+        return self._has(self.tree, phrase[:])
